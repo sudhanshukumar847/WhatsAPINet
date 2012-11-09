0ddb35f897d1302e6bd9057eebf202cb9caadc38
Luis Rodriguez <luis.rodriguez@opendeusto.es>
2012-11-09 15:04:43 +0100

    Added some encryption tests. One is WIP and doesn't pass. Not yet determined whether that's correct behaviour

--- src/WhatsUnitTest/EncryptionRelatedTests.cs
+++ src/WhatsUnitTest/EncryptionRelatedTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 
 using WhatsAppApi.Helper;
+using System.Collections.Generic;
 
 namespace WhatsUnitTest {
     [TestFixture]
@@ -31,6 +32,29 @@ namespace WhatsUnitTest {
             var a3 = Encryption.WhatsappEncrypt(TestKey, Util.StringToByteArray(messages[2]), true);
             var aa3 = Util.ByteArrayToString(a3);
             Assert.AreEqual(aa3.ToUpper(), answers[2]);
+        }
+
+        [Test]
+        public void EncryptionDecryptionTest()
+        {
+
+            string[] messages = { "D814F2CE24AF7153B37BEEB20149BA669436172C8655BC09ACE66B23CEC8C0EA",
+                                         "D814F2CE24AF7153B37BEEB20149BA669436172C8655BC09ACE66B23CEC8C0EA",
+                                         "D814F2CE24AF7153B37BEEB20149BA669436172C8655BC09ACE66B23CEC8C0EA" };
+            string[] answers = {"F227B9F6D30231288B0FE60A466B343C6B550A89778F71F16916E5066E9268208498686F",
+                                    "EAC74BC62C02AB7D724DE2382A51CD8834C804DC49DCEF7CDBAE2E5E2E43D3789BBF3AD0",
+                                    "C7EDB8049277F0B20ABFF211907B829397EB9144D522B6AA40CB53B410D869FE747A10AD"};
+
+            Encryption._Reset();
+
+            dynamic vars = new Dictionary<string, object>();
+
+            var e1 = Encryption.WhatsappEncrypt(TestKey, Util.StringToByteArray(messages[0]), true);
+            var r1 = Encryption.WhatsappDecrypt(TestKey, e1);
+            var r1s = Util.ByteArrayToString(r1).ToUpper();
+            Assert.AreEqual(messages[0], r1s);
+
+
         }
 
     } //! class
