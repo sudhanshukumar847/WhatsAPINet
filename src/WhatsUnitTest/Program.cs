a0fd628804590b71b7285a119f9b0290fa78e6e0
Luis Rodriguez <luis.rodriguez@opendeusto.es>
2012-11-09 14:33:28 +0100

    Added nunit console auto runner to WhatsUnitTest main. Tests can still be run through Visual Nunit

--- src/WhatsUnitTest/Program.cs
+++ src/WhatsUnitTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 
 namespace WhatsUnitTest
 {
@@ -9,6 +10,10 @@ namespace WhatsUnitTest
     {
         static void Main(string[] args)
         {
+            NUnit.ConsoleRunner.Runner.Main(new string[]
+           {
+              Assembly.GetExecutingAssembly().Location, 
+           });
         }
     }
 }
